c6c9b4a7d34563fa0aec5e8ac370e92d53204a03
Gonr <one642077@gmail.com>
2023-06-08 18:57:08 +0300

    1.1.4 add editing docs and save docs

--- src/page/Docs.jsx
+++ src/page/Docs.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import {
+	collection,
+	doc,
+	getDoc,
+	onSnapshot,
+	updateDoc,
+} from 'firebase/firestore'
+import React, { useEffect, useState } from 'react'
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
 import { useParams } from 'react-router-dom'
+import { db } from '../firebase'
 
 function Docs() {
 	const params = useParams()
+	const [editorData, setEditorData] = useState('')
+
+	useEffect(() => {
+		const document = onSnapshot(
+			doc(collection(db, 'docs-data'), params.id),
+			res => {
+				setEditorData(res.data().body)
+			}
+		)
+		return () => document
+	}, [])
+
+	function handleChange(value) {
+		setEditorData(value)
+	}
+	useEffect(() => {
+		const updateDocument = setTimeout(() => {
+			updateDoc(doc(collection(db, 'docs-data'), params.id), {
+				body: editorData,
+			})
+		}, 1000)
+		return () => clearTimeout(updateDocument)
+	}, [editorData])
+
 	return (
 		<div>
-			<h1>qwe</h1>
+			<ReactQuill theme='snow' value={editorData} onChange={handleChange} />
 		</div>
 	)
 }
