9f17bef6367ab954af750375a5ef3081729ac1d5
Gonr <one642077@gmail.com>
2023-06-12 16:28:06 +0300

    fix to delpoy

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app'
-import { getAnalytics } from 'firebase/analytics'
+// import { getAnalytics } from 'firebase/analytics'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
@@ -14,7 +14,7 @@ const firebaseConfig = {
 }
 
 const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
+// const analytics = getAnalytics(app)
 //
 export const auth = getAuth(app)
 export const db = getFirestore(app)
